aa427a1c691d9f47c0f59a058f2a1e8fb7a855a2
Harold Camargo <hscamargo2002@gmail.com>
2023-04-04 10:09:37 -0500

    ✨

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     //if you already have other plugin just paste this lines below
+//     [
+//       'module-resolver',
+//       {
+//         root: ['./src'],
+//         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+//         alias: {
+//           '@components': './src/components',
+//         },
+//       },
+//     ],
+//   ],
+// };
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@perro': './commons',
+          },
+        },
+      ],
+    ],
+  };
+};
